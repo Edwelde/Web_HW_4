bdd517103f6dadc0e5d127e0e139792378b46651
Edwelde <46779961+Edwelde@users.noreply.github.com>
2020-01-20 21:57:31 +1000

    hw4 init

---
+++ hm_4/Models/MenuContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace hm_4.Models
+{
+    public class MenuContext : DbContext
+    {
+        public DbSet<Moderator> ModeratorList { get; set; }
+        public DbSet<Admin> AdminList { get; set; }
+
+        public MenuContext(DbContextOptions<MenuContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MenuContext;Username=postgres;Password=1qasw23ed");
+        }
+    }
+}
---
+++ hm_4/SampleData.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using hm_4.Models;
+
+namespace hm_4
+{
+    public static class SampleData
+    {
+        public static void Initialize(MenuContext context)
+        {
+            if (!context.AdminList.Any())
+            {
+                context.AdminList.AddRange(
+                new Admin
+                {
+                    FirstName = "ктота",
+                    SecondName = "какойта",
+                    Age = 666,
+                    City = "гдета"
+                },
+                new Admin
+                {
+                    FirstName = "чел",
+                    SecondName = "челочич",
+                    Age = 444,
+                    City = "человековск"
+                },
+                new Admin
+                {
+                    FirstName = "Митяй",
+                    SecondName = "Минин",
+                    Age = 18,
+                    City = "ВДК"
+                }
+                );
+                context.SaveChanges();
+            }
+
+            if (!context.ModeratorList.Any())
+            {
+                context.ModeratorList.AddRange(
+                new Moderator
+                {
+                    FirstName = "Модер",
+                    SecondName = "Модерович",
+                    Age = 1,
+                    Experience = 1000
+                },
+                new Moderator
+                {
+                    FirstName = "Каратель228",
+                    SecondName = "ТвоюМа",
+                    Age = 10,
+                    Experience = 10
+                }
+                );
+
+            context.SaveChanges();
+            }
+        }
+    }
+}
